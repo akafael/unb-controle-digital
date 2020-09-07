b7abed3fb30d95110623a0df10a624389fa12ab3
Rafael <akafael@users.noreply.github.com>
2020-09-07 19:35:43 -0300

    Add exsim1 simulation results from octave

--- src/matlab/exsim1.m
+++ src/matlab/exsim1.m
@@ -1,16 +1,69 @@
 %% EXSIM1 - Digital Control Experiment
 % @author Rafael Lima
 
-pkg load control % Required for octave
+% Get path from current file
+file_path = fileparts(mfilename('fullpath'))
 
 %% Discrete Transfer Function definition
 Ts = 1
-Gnum = [0.3679 0.2642]
-Gden = conv([1 -1],[1 -0.3679])
+K = 1
+a1 = 0.3679
+a0 = 0.2642
+b1 = 1
+Gnum = [a1 a0]*K
+Gden = conv([1 -b1],[1 -a1])
 G = tf(Gnum,Gden,Ts)
+Gc = feedback(G)
+%grid off
+%step(Gc)
+
+%% Oscilation
+Ts = 1
+K = (1 - a1*b1)/a0 % Expression from Juri Stability Criteria Result
+Gnum = [a1 a0]*K
+Gden = conv([1 -b1],[1 -a1])
+G = tf(Gnum,Gden,Ts)
+Gc = feedback(G)
+
+% Plot Figure
+fig = figure();
+grid off;
+step(Gc);
+title(["K = ", num2str(K)])
+print(fig, [file_path,"/../tex/img/exsim1-plot-oscilation.pdf"],"-dpdflatex")
+
+%% Instable
+Ts = 1
+K = 2*(1 - a1*b1)/a0
+Gnum = [a1 a0]*K
+Gden = conv([1 -b1],[1 -a1])
+G = tf(Gnum,Gden,Ts)
+Gc = feedback(G)
+
+% Plot Figure
+fig = figure();
+grid off;
+step(Gc);
+title(["K = ", num2str(K)])
+print(fig, [file_path,"/../tex/img/exsim1-plot-instable.pdf"],"-dpdflatex")
+
+%% Stable
+Ts = 1
+K = 0.5*(1 - a1*b1)/a0
+Gnum = [a1 a0]*K
+Gden = conv([1 -b1],[1 -a1])
+G = tf(Gnum,Gden,Ts)
+Gc = feedback(G)
+
+% Plot Figure
+fig = figure();
+grid off;
+step(Gc);
+title(["K = ", num2str(K)])
+print(fig, [file_path,"/../tex/img/exsim1-plot-stable.pdf"],"-dpdflatex")
 
 %% Symbolic Representation
-    %syms z
+%syms z
 %sGnum = poly2sym(Gnum,z)
 %sGden = poly2sym(Gden,z)
 %sG = sGnum/sGden
@@ -33,8 +86,4 @@ G = tf(Gnum,Gden,Ts)
 %k = 1
 %a = Gden
 %mb = [[a(1) a(n-k)];[a(n) a(k)]]
-
-%% Graphical Evaluation
-%rlocus(G)
-%zgrid
 
