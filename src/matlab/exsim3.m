44c528f2e9902392c1540d8f56e277fdc3204e9b
Rafael <akafael@users.noreply.github.com>
2020-09-21 19:48:28 -0300

    Add exsim3 controler gain calculus

--- src/matlab/exsim3.m
+++ src/matlab/exsim3.m
@@ -12,6 +12,8 @@ tex_path = strcat(file_path,"/../tex/aux/")
 % Define Symbols
 syms s z T w K
 
+%% Part 1
+
 % Transfer Function
 Ts = 0.1
 a0 = 1
@@ -49,7 +51,7 @@ for Ts = [0.5 1 2]
     fig = figure()
     rlocus(Gz)
     xlim([-1.5 1])
-    title(strcat("Root Locus (T=",num2str(Ts),")"))
+    title(strcat("Root Locus (T=",num2str(Ts),"s)"))
     print(fig, strcat(img_path,"exsim3-rlocus-t",num2str(Ts*1000),"ms.png"),"-dpng")
     
     % Find Max Stable K
@@ -66,7 +68,7 @@ for Ts = [0.5 1 2]
     Gcc = feedback(Gz,Ts)
     fig = figure()    
     step(Gcc)
-    title(strcat("Step Response (T=",num2str(Ts),")"))
+    title(strcat("Step Response (T=",num2str(Ts),"s)"))
     print(fig, strcat(img_path,"exsim3-step-t",num2str(Ts*1000),"ms.png"),"-dpng")
 
     % Step Response
@@ -76,7 +78,10 @@ for Ts = [0.5 1 2]
     damping = log(sysinfo.Overshoot)/sqrt(pi^2+(log(sysinfo.Overshoot)^2))
 
     % Print System Response to tex table
-    fprintf(fileTable,"%.1f & %.3f & %.3f & %.3f\\\\\n",Ts, sysinfo.Overshoot, damping , sysinfo.SettlingTime);
+    fprintf(fileTable,"%.1f & (%s, %s) & %.2f & %.3f & %.2f\\\\\n",...
+            Ts,...
+            num2str(poles2(1)), num2str(poles2(2)),...
+            sysinfo.Overshoot/100, damping , sysinfo.SettlingTime);
 end
 
 fprintf(fileTable,"\n")
@@ -99,32 +104,65 @@ sG2 = poly2sym(Gnum,s)/poly2sym(Gden,s);
 partfrac((1/s)*sG2)  % Calculate partial fraction
 
 Gz2 = c2d(G2,Ts,'zoh')
+sGz2 = poly2sym(Gz2.num{1},z)/poly2sym(Gz2.den{1},z);
+
+fileEq = fopen(strcat(tex_path,"exsim3-eq-g2-tf.tex"),"w")
+fprintf(fileEq,"G(z),%s\n",latex(sGz2))
+fclose(fileEq)
+
+% Request Conditions
+desiredDamping = 0.5
+desiredSettlingTime = 2
+desiredOvershoot = exp(-desiredDamping*pi/sqrt(1-desiredDamping^2))
+desiredPoles = (4/desiredSettlingTime)*(1 + i*[1 -1]*sqrt(1-desiredDamping^2)/desiredDamping)
+desiredPolesZ = exp(-desiredPoles*Ts)
 
 % Controler
-sGc = (z-0.6703)/(z-0.1)
+%sGc = (z-exp(-2*Ts))/(z-0.25)
+sGc = (z-exp(-2*Ts))/(z-0.776)
 [num,den] = numden(subs(sGc,T,Ts))
 Gc = tf(sym2poly(num),sym2poly(den),Ts)
-Gmf2 = feedback(Gc*Gz2,1)
+
+sGma2 = simplify(expand(K*sGc*sGz2))
+sGmf2 = simplify(expand(sGma2/(1+sGma2)))
+
+% Find Max K for stable function
+[~,poly2] = numden(sGmf2)
+Kd = abs(eval(subs(solve(poly2,K),z,desiredPolesZ(1))))
+
+Gmf2 = feedback(Gc*Gz2,Kd)
 
 % Root Locus
 fig = figure()
+hold on;
 rlocus(Gz2)
 zgrid()
+plot(desiredPolesZ,'*')
 xlim([-3 1.5])
+hold off;
 print(fig, strcat(img_path,"exsim3-rlocus-g2.png"),"-dpng")
 
 % Root Locus Controler
 fig = figure()
+hold on;
 rlocus(Gz2*Gc)
 zgrid()
+plot(desiredPolesZ,'*')
 xlim([-3 1.5])
+hold off;
 print(fig, strcat(img_path,"exsim3-rlocus-g2-control.png"),"-dpng")
+
+% Step Response Evaluation
+sysinfo2 = stepinfo(Gmf2)
+qsi = log(sysinfo2.Overshoot)/sqrt(pi^2+(log(sysinfo2.Overshoot)^2))
 
 % Step Response
 fig = figure()
+hold on;
 step(Gmf2)
-sysinfo2 = stepinfo(Gmf2)
-qsi = log(sysinfo2.Overshoot)/sqrt(pi^2+(log(sysinfo2.Overshoot)^2))
+%yline(desiredOvershoot+sysinfo2.SettlingMax,':')
+xline(desiredSettlingTime,':')
+legend("G","")
+hold off;
 print(fig, strcat(img_path,"exsim3-step-g2-control.png"),"-dpng")
 
-close all
--- src/python/exsim3.py
+++ src/python/exsim3.py
@@ -31,12 +31,19 @@ def partfrac(G,s):
 
     return Gp
 
+def roundExpr(expr, num_digits=4):
+    """
+    Round Every Number in an expression
+    """
+    return expr.xreplace({n : round(n, num_digits) for n in expr.atoms(Number)})
+
+
 printing.printer.Printer().set_global_settings(precision=3)
 
 # Symbols
 s = symbols("s",complex=True)
 z = symbols("z",complex=True)
-K,a1,a0,T = symbols("K alpha_1 alpha_0 T",real=True)
+K,a1,a0,T,b = symbols("K alpha_1 alpha_0 T beta",real=True)
 
 # Constants
 na0 = 1
@@ -46,7 +53,7 @@ nT = 0.5
 sGo = 1/(s+na0)
 
 # Z Transform (From table) 
-sGz = (1-exp(-T))/(z-exp(-T))
+sGz = (1/na0)*(1-exp(-T))/(z-exp(-T))
 
 # Controler TF
 sGc = K*z/(z-1)
@@ -61,3 +68,49 @@ sK = solve(poly,K)[0]
 Kmax = sK.subs([(T,nT),(z,-1)])
 
 poles2 = solve(poly.subs([(T,nT),(K,2)]),z)
+
+
+# Part 2
+# Constants
+na1 = 2
+Ts = 0.2
+
+# Open Loop Transfer Function
+sGo2 = 1/(s+2)
+
+# Z Transform (From table) 
+sGz2 = (z-1)*(1/(4*(z-exp(-2*T))) - 1/(4*(z-1)) + T*(1/((z-1)*(z-1))))
+
+# Controler TF
+sGc2 = K*(z-exp(-na1*T))/(z-b)
+sGma2 = simplifyFraction(sGc2*sGz2,z)
+sGmf2 = simplify(expand(sGma2/(1+sGma2))) 
+
+# Characterist Equation
+_,poly2 = fraction(sGmf2)
+
+# Request Conditions
+desiredDamping = 0.5
+desiredSettlingTime = 2
+desiredOvershoot = exp(-desiredDamping*pi/sqrt(1-desiredDamping**2))
+desiredPoles = [0,0]
+desiredPoles[0] = -(4/desiredSettlingTime)*(1 + I*sqrt(1-desiredDamping**2)/desiredDamping)
+desiredPoles[1] = -(4/desiredSettlingTime)*(1 - I*sqrt(1-desiredDamping**2)/desiredDamping)
+desiredPolesZ = [exp(desiredPoles[0]*Ts),exp(desiredPoles[1]*Ts)]
+
+# Solve Linear System to find K and b
+sysKb = [K,b]
+sysKb[0] = poly2.subs([(z,desiredPolesZ[0]),(T,Ts)]).evalf().collect(K).collect(b)
+sysKb[1] = poly2.subs([(z,desiredPolesZ[1]),(T,Ts)]).evalf().collect(K).collect(b)
+resp = list(linsolve(sysKb,(K,b)))[0]
+nK = resp[0]
+nb = resp[1]
+
+# Find TF
+nGmf2 = sGmf2.subs([(K,nK),(b,nb),(T,Ts)])
+
+# Find Critical Value for K
+sK2 = solve(poly2,K)[0]
+#Kmax = sK.subs([(T,nT),(z,-1)])
+
+
