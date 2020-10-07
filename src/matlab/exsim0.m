33f905dc87febeb1ac377116df64f73a883fd35f
Rafael <akafael@users.noreply.github.com>
2020-10-07 03:52:08 -0300

    Add q3 listp1

--- src/matlab/exsim0.m
+++ src/matlab/exsim0.m
@@ -15,42 +15,42 @@ img_path = strcat(file_path,"/../../tex/img/")
 syms s z T
 
 % Coeficients 
-Ts = 0.1
-K0 = 5
-K1 = 3.2
-a0 = 1
-a1 = 4
+q1Ts = 0.1
+q1K0 = 5
+q1K1 = 3.2
+q1a0 = 1
+q1a1 = 4
 
 %%
 % Transfer Function
-G = tf(K0,[1 a0 0])
-Gc = tf(K1*[1 a0],[1 a1])
+q1G = tf(q1K0,[1 q1a0 0])
+q1Gc = tf(q1K1*[1 q1a0],[1 q1a1])
 
 %%
 % Symbolic Expressions
-sG = poly2sym(K0,s)/poly2sym([1 a0 0],s)
-sGc = poly2sym(K1*[1 a0],s)/poly2sym([1 a1],s)
+q1sG = poly2sym(q1K0,s)/poly2sym([1 q1a0 0],s)
+q1sGc = poly2sym(q1K1*[1 q1a0],s)/poly2sym([1 q1a1],s)
 
 %%
 % Find Closed Loop Transfer Function
-Gma = G*Gc
-Gmf = feedback(Gma,1)
+q1Gma = q1G*q1Gc
+q1Gmf = feedback(q1Gma,1)
 
 %%
 % Find Partial Frac Expression
-partfrac(sG/s)
+partfrac(q1sG/s)
 
 %%
 % From z Transform table
-sGz = K0*(1-z^(-1))*((z/(z-exp(-a0*T))) - (z/(z-1)) + (T*z/((z-a0)^2)))
-sGz = collect(simplify(expand(sGz)),z)
+q1sGz = q1K0*(1-z^(-1))*((z/(z-exp(-q1a0*T))) - (z/(z-1)) + (T*z/((z-q1a0)^2)))
+q1sGz = collect(simplify(expand(q1sGz)),z)
 
 % Simplify
-vpa(simplify(expand(subs(sGz,T,Ts))),4)
+vpa(simplify(expand(subs(q1sGz,T,q1Ts))),4)
 
 %%
 % ZOH
-Gz = c2d(G,Ts,'zoh')
+q1Gz = c2d(q1G,q1Ts,'zoh')
 
 
 %% QUESTION 1b
@@ -59,77 +59,156 @@ Gz = c2d(G,Ts,'zoh')
 % Forward Rectangle
 
 % Find Transfer Function
-Gzf = (z - 1)/T
-sGcf = subs(sGc,s,Gzf)
-sGcf = expand(simplify(sGcf))
+q1Gzf = (z - 1)/T
+q1sGcf = subs(q1sGc,s,q1Gzf)
+q1sGcf = expand(simplify(q1sGcf))
 
 % Convert Simbolic Expression to TF
-[num,den] = numden(subs(sGcf,T,Ts))
+[num,den] = numden(subs(q1sGcf,T,q1Ts))
 num = sym2poly(num)
 den = sym2poly(den)
-Gcf = tf(num,den,Ts)
+q1Gcf = tf(num,den,q1Ts)
 
 %%
 % ZOH Method
-Gcz = c2d(Gc,Ts,'zoh')
+q1Gcz = c2d(q1Gc,q1Ts,'zoh')
 
 %%
 % Mached Poles Method
-Gcm = c2d(Gc,Ts,'matched')
+q1Gcm = c2d(q1Gc,q1Ts,'matched')
 
 %% QUESTION 1c
 
 %%
 % Find Desired Poles
-MP = 0.163333
-tc = 2
+q1MP = 0.163333
+q1tc = 2
 
-qsi = -log(MP)/sqrt(pi^2 + (log(MP))^2) % From MP
-qsi = 0.5 % From TF
-w0 = tc/qsi
+q1qsi = -log(q1MP)/sqrt(pi^2 + (log(q1MP))^2) % From MP
+q1qsi = 0.5 % From TF
+q1w0 = q1tc/q1qsi
 
-DesiredPoles = roots([1 2*tc w0^2])
-DesiredPolesZ = exp(DesiredPoles*Ts)
+q1DesiredPoles = roots([1 2*q1tc q1w0^2])
+q1DesiredPolesZ = exp(q1DesiredPoles*q1Ts)
 
 
 %% QUESTION 1d
 
 %%
 %
-zpk(feedback(Gcm*Gz,1))
+zpk(feedback(q1Gcm*q1Gz,1))
 
-polesFm = rlocus(Gcm*Gz,1)
+q1polesFm = rlocus(q1Gcm*q1Gz,1)
 
-rlocus(Gcm*Gz);
+rlocus(q1Gcm*q1Gz);
 hold on;
-plot(DesiredPolesZ,'*')
+plot(q1DesiredPolesZ,'*')
 ylim(3*[-1 1]);
 xlim(3*[-1 1]);
 hold off;
 
 %%
 %
-zpk(feedback(Gcf*Gz,1))
+zpk(feedback(q1Gcf*q1Gz,1))
 
-polesFf = rlocus(Gcf*Gz,1)
+q1polesFf = rlocus(q1Gcf*q1Gz,1)
 
-rlocus(Gcf*Gz);
+rlocus(q1Gcf*q1Gz);
 hold on;
-plot(DesiredPolesZ,'*')
+plot(q1DesiredPolesZ,'*')
 ylim(3*[-1 1]);
 xlim(3*[-1 1]);
 hold off;
 
 %%
 %
-zpk(feedback(Gcz*Gz,1))
+zpk(feedback(q1Gcz*q1Gz,1))
 
-polesFz = rlocus(Gcz*Gz,1)
+q1polesFz = rlocus(q1Gcz*q1Gz,1)
 
-rlocus(Gcz*Gz);
+rlocus(q1Gcz*q1Gz);
 hold on;
-plot(DesiredPolesZ,'*')
+plot(q1DesiredPolesZ,'*')
 ylim(3*[-1 1]);
 xlim(3*[-1 1]);
 hold off;
 
+%%
+%
+
+step(feedback(q1G*q1Gc,1))
+hold on;
+step(feedback(q1Gcm*q1Gz,1),'--')
+step(feedback(q1Gcf*q1Gz,1),'-.')
+step(feedback(q1Gcz*q1Gz,1),'.')
+legend('Continuos','Matched','Forward','ZOH')
+hold off;
+
+%% QUESTION2
+
+close all
+
+syms s z T Kc D a b K2 D
+
+% Coeficients
+q2Ts = 0.2
+q2na = 0.6
+q2nb = 1.5
+q2K2 = 2
+
+% Expressions
+q2sG1 = a/(s+a)
+q2sG2 = q2K2/(s+b)
+q2sGc = Kc
+q2sQ = D
+q2sGho = 1/s
+
+q2sGma0 = (q2sGc*q2sG1*q2sGho+q2sQ)*q2sG2
+q2sGma1 = subs(q2sGma0,[a b],[q2na q2nb])
+
+%%
+%
+pretty(vpa(partfrac(q2sGma1),4))
+
+%%
+% From Table
+q2sGmaz = (1-z^(-1))*(((4*Kc)/3)*(z/(z-1)) - ((100*Kc)/(9*5))*(z/(z-exp(-(3/5)*T))) + ((4*D + (16*Kc)/9)/2)*(z/(z-exp(-(3/2)*T))))
+
+pretty(q2sGmaz)
+
+pretty(simplifyFraction(q2sGmaz))
+
+%% QUESTION 2c
+
+%%
+% SteadState Error
+q2exprLimErrorStep = collect(subs(q2sGmaz,[T D],[q2Ts 0]),z)
+q2exprErrorStep = subs(q2exprLimErrorStep,z,0)
+
+q2DesiredErrorStep = 0.1
+q2nKc = eval(solve(q2exprErrorStep - q2DesiredErrorStep,Kc))
+
+%% QUESTION3
+
+syms s z T Kc Td
+
+% Coefficients
+q3Ts = 1
+
+% Transfer Function
+q3G = tf(1,[1 0 0])
+
+sGz = (T^2)*((z+1)/(2*(z-1)^2))
+
+sGmf = 1/((1+Td - z^(-1))/Kc + 1/(Kc*sGz))
+sGmf = collect(simplify(expand(sGmf)),z)
+
+%% QUESTION 3b
+q3MP = 0.2
+q3w0 = (2*pi)/(12*q3Ts)
+q3qsi = -log(q1MP)/sqrt(pi^2 + (log(q1MP))^2) % From MP
+
+q3tc = q3qsi*q3w0
+
+q3DesiredPoles = roots([1 2*q3tc q3w0^2])
+q3DesiredPolesZ = exp(q3DesiredPoles*q3Ts)
